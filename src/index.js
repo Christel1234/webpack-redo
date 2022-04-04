625934222cc9d75d3d707043c1c936742dd74fb7
Christel1234 <christel.jansenvanrensburg@yoco.com>
2022-04-04 15:05:40 +0200

    Output Management-Preparation

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import _ from "lodash";
 import "./style.css";
 import Icon from "./icon.png";
+import printMe from "./print.js";
 
 function component() {
   const element = document.createElement("div");
@@ -14,6 +15,14 @@ function component() {
   myIcon.src = Icon;
 
   element.appendChild(myIcon);
+
+  // New print file
+  const btn = document.createElement("button");
+
+  btn.innerHTML = "Click me and check the console!";
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
 
   return element;
 }
